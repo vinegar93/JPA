4eee73199eb44e3bb83144c98ea09771e4c7bc8a
vinegar93 <vinegar93@163.com>
2017-10-10 13:37:18 +0800

    ManyToOne关系学习笔记

---
+++ JPA07/src/cn/hwd/jpa/test/ManyToOneTest.java
@@ -0,0 +1,35 @@
+package cn.hwd.jpa.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+import cn.hwd.jpa.entity.Department;
+import cn.hwd.jpa.entity.Employee;
+
+public class ManyToOneTest {
+
+	@Test
+	public void save() {
+		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("JPA07");
+	   	EntityManager entitymanager = emfactory.createEntityManager( );
+	   	entitymanager.getTransaction( ).begin( );
+	   	Department department = new Department();
+	   	department.setName("Development");
+	   	entitymanager.persist(department);
+	   	Employee employee1 = new Employee();
+	   	employee1.setName("Tom");
+	   	employee1.setDepartment(department);
+	   	entitymanager.persist(employee1);
+	   	Employee employee2 = new Employee();
+	   	employee2.setName("Jerry");
+	   	employee2.setDepartment(department);
+	   	entitymanager.persist(employee2);
+	   	entitymanager.getTransaction().commit();
+	   	entitymanager.close();
+	   	emfactory.close();
+	}
+
+}
